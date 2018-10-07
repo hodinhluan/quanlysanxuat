7ba0b801551f5e63f4024eb368ecb531c2690527
Sanh Phan Van <sanhphanvan96@gmail.com>
2018-10-07 17:47:47 +0700

    feat(user): validate unique username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :groups
 
-  validates :name, presence: true, length: {maximum: 50}
+  validates :name, presence: true, length: {maximum: 50}, uniqueness: {case_sensitive: false}
   validates :email, presence: true, length: {maximum: 50}
   validates :password, presence: true
   validates :password_confirmation, presence: true
