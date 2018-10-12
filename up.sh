4437004016ba4628cb9541060dc61a1463bc591a
Sanh Phan Van <sanhphanvan96@gmail.com>
2018-10-12 13:17:15 +0700

    feat(docker): auto migrate/seeds db

--- up.sh
+++ up.sh
@@ -3,12 +3,12 @@
 # just keep going if we don't have anything to install
 set +e
 
-# export secret before anything
-source /app/.profile.d/ruby.sh
-
 # setup database
 rails db:migrate
 rails db:setup
+
+# run server
+rails server
 
 set -e
 
